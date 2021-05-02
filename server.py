e6ccc41a0f32d186bdd08f2665e9d6f176eb338a
Igor <i.v.olkhovsky@gmail.com>
2021-05-02 22:48:46 +0300

    add history feature in backend

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
+from fastapi.responses import HTMLResponse, JSONResponse, FileResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+import io
 import uvicorn
 
 from backend.face_detector import FaceDetector
-from utils import decode_image
+from utils import decode_image, init_repo, save_sample, get_processed_samples_list, read_sample, encode_image
 
 processor = FaceDetector()
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+init_repo()
 
 origins = [
     "http://localhost",
@@ -36,23 +39,51 @@ async def root(request: Request):
     return FileResponse("static/ui.html")
 
 
+@app.get("/samples", response_class=JSONResponse)
+async def root(request: Request):
+    return JSONResponse(get_processed_samples_list())
+
+
+@app.get("/image", response_class=JSONResponse)
+async def root(request: Request):
+    processed_imgs = get_processed_samples_list()
+    sample = processed_imgs[-1]
+    if "sample_id" in request.query_params.keys():
+        sample = request.query_params["sample_id"]
+    img, report = read_sample(sample)
+    encoded = encode_image(img)
+    return StreamingResponse(io.BytesIO(encoded.tobytes()), media_type="image/jpg")
+
+
+@app.get("/report", response_class=JSONResponse)
+async def root(request: Request):
+    processed_imgs = get_processed_samples_list()
+    sample = processed_imgs[-1]
+    if "sample_id" in request.query_params.keys():
+        sample = request.query_params["sample_id"]
+    img, report = read_sample(sample)
+    return JSONResponse(report)
+
+
 @app.post("/image")
 async def process_image(file: UploadFile = File(...)):
     content = await file.read()
     img = decode_image(content)
-    img, data = processor(img)
+    _, data = processor(img)
     data = [{
         "label": f"face#{idx}",
         "score": 1.0,
         "bbox": [int(x) for x in box],
     } for idx, box in enumerate(data)]
-    return JSONResponse({
+    processing_result = {
         "id": file.filename,
         "file_size": len(content),
         "width": img.shape[1],
         "height": img.shape[0],
         "detections": data
-    })
+    }
+    save_sample(file.filename, img, processing_result)
+    return JSONResponse(processing_result)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,6 +1,74 @@
 import cv2
+from glob import glob
 import numpy as np
+from os.path import isdir, join, basename, splitext, getmtime
+from os import makedirs, remove
+from pathlib import Path
+import yaml
+
+
+DEFAULT_REPO_ROOT = "repo"
+DEFAULT_IMGS_FOLDER = "images"
+DEFAULT_YMLS_FOLDER = "reports"
+DEFAULT_MAX_SAMPLES_TO_STORE = 3
 
 
 def decode_image(content):
     return cv2.imdecode(np.frombuffer(content, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+
+def encode_image(image):
+    res, encoded = cv2.imencode(".jpg", image)
+    return encoded
+
+
+def write_yaml(data, path):
+    with open(path, "w") as f:
+        yaml.dump(data, f)
+
+
+def read_yaml(path):
+    with open(path, "r") as f:
+        return yaml.load(f, Loader=yaml.SafeLoader)
+
+
+def init_repo():
+    for target in [DEFAULT_REPO_ROOT,
+                   join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER),
+                   join(DEFAULT_REPO_ROOT, DEFAULT_YMLS_FOLDER)]:
+        if not isdir(target):
+            makedirs(target)
+
+
+def save_sample(name, img, report):
+    samples = get_processed_samples_list()
+
+    while len(samples) >= DEFAULT_MAX_SAMPLES_TO_STORE:
+        oldest_sample = samples[0]
+        remove(join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER, oldest_sample + ".jpg"))
+        remove(join(DEFAULT_REPO_ROOT, DEFAULT_YMLS_FOLDER, oldest_sample + ".yml"))
+        samples.pop(0)
+
+    name = basename(splitext(name)[0])
+    cv2.imwrite(join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER, name + ".jpg"), img)
+    write_yaml(report, join(DEFAULT_REPO_ROOT, DEFAULT_YMLS_FOLDER, name + ".yml"))
+
+
+def read_sample(name):
+    return cv2.imread(join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER, name + ".jpg")), \
+           read_yaml(join(DEFAULT_REPO_ROOT, DEFAULT_YMLS_FOLDER, name + ".yml"))
+
+
+def get_processed_samples_list():
+    image_files = {splitext(basename(x))[0] for x in glob(join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER, "*.jpg"))}
+    yaml_files = {splitext(basename(x))[0] for x in glob(join(DEFAULT_REPO_ROOT, DEFAULT_YMLS_FOLDER, "*.yml"))}
+    valid_samples = image_files.intersection(yaml_files)
+    for path in image_files:
+        if path not in valid_samples:
+            remove(join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER, path + ".jpg"))
+    for path in yaml_files:
+        if path not in valid_samples:
+            remove(join(DEFAULT_REPO_ROOT, DEFAULT_YMLS_FOLDER, path + ".yml"))
+    samples = [splitext(x.name)[0] for x in
+               sorted(Path(join(DEFAULT_REPO_ROOT, DEFAULT_IMGS_FOLDER)).iterdir(), key=getmtime)]
+    return samples
